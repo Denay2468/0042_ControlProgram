253c3fc82ea583156b564b940d76cab82ea798fb
Denay2468 <denayaung13@gmail.com>
2026-03-31 12:14:18 +0630

    Add GraduationStatus function with if-else

--- ElseIf.cpp
+++ ElseIf.cpp
@@ -6,6 +6,14 @@ float Average(float a, float b){
     return (a+b)/2;
 }
 
+// Function to determine pass or fail using if-else
+string GraduationStatus(float r){
+    if (r >= 60)
+        return "Passed";
+    else
+        return "Failed";
+}
+
 int main(){
 
 }
