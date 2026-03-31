3a7c42e4eaf250a6e5ee2b83b142710cf7864260
Denay2468 <denayaung13@gmail.com>
2026-03-31 12:13:45 +0630

    Add Average function

--- ElseIf.cpp
+++ ElseIf.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    // Function to calculate average of two scores
+// Function to calculate average of two scores
 float Average(float a, float b){
     return (a+b)/2;
 }
+
+int main(){
 
 }
