e30ee5947f3381845fb93ae975ab1d290fd19d7a
Denay2468 <denayaung13@gmail.com>
2026-03-31 12:19:08 +0630

    Add comments to all functions and variables

--- ElseIf.cpp
+++ ElseIf.cpp
@@ -1,24 +1,27 @@
+// Program to demonstrate if-else statement
 #include <iostream>
 using namespace std;
 
 // Function to calculate average of two scores
 float Average(float a, float b){
-    return (a+b)/2;
+    return (a+b)/2; // Add both scores and divide by 2
 }
 
 // Function to determine pass or fail using if-else
 string GraduationStatus(float r){
+    // If average is 60 or above, student passes
     if (r >= 60)
         return "Passed";
+    // Otherwise student fails
     else
         return "Failed";
 }
 
 int main(){
-    float engScore, mathScore, average;
-    string status;
+    float engScore, mathScore, average; // Variables for scores and average
+    string status; // Variable to store graduation status
 
-        // Input section
+    // Input section
     cout << "Enter English score = ";
     cin >> engScore;
     cout << "Enter Math score = ";
@@ -30,3 +33,4 @@ int main(){
 
     return 0;
 }
+// End of ElseIf program
