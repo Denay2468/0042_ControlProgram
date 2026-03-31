7634743cb4984bc3c3a394a7e287e7bb28d6edcd
Denay2468 <denayaung13@gmail.com>
2026-03-31 12:03:50 +0630

    Add GraduationStatus function with nested if

--- NestedIf.cpp
+++ NestedIf.cpp
@@ -6,6 +6,19 @@ float Average(float a, float b){
     return (a+b)/2;
 }
 
+// Function to determine graduation status using nested if
+string GraduationStatus(float r){
+    if (r >= 60)
+        if(r > 80){
+            return "Graduated with Distinction";
+        }
+        else{
+            return "Graduated with Good Standing";
+        }
+    else
+        return "Failed";
+}
+
 int main(){
 
 }
