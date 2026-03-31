79d9fa0befe80bad4c6f689bdb6940c60e64c12d
Denay2468 <denayaung13@gmail.com>
2026-03-31 11:54:35 +0630

    Add comments to all lines

--- SimpleIf.cpp
+++ SimpleIf.cpp
@@ -1,19 +1,21 @@
+// Program to demonstrate simple if statement
 #include <iostream>
 using namespace std;
 
 int main(){
-    int x;
+    int x; // Variable to store random number
 
-    srand(time(0));
-    x = rand() % 10;
+    srand(time(0)); // Set random seed based on current time
+    x = rand() % 10; // Generate random number between 0-9
 
+    // Check if x equals 5, if so correct the value
     if (x == 5){
-        x = x + 1;
+        x = x + 1; // Add 1 to avoid value 5
         cout << "x value has been corrected to : " << x << endl;
     }
 
+    // Display the final number
     cout << "The given number = " << x;
 
     return 0;
-
 }
